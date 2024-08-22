7688071cec86030d20eb0f992de65460546f0905
nicksmailer <nicksmailer@verizon.net>
2024-08-22 15:51:13 -0400

    Initial commit

---
+++ Website.java
@@ -0,0 +1,8 @@
+public class Website {
+    public static void main (String[] args){
+        git remote add origin https://github.com/nicksmailer/My-Projects.git
+        git branch -M main;
+        git push -u origin main;
+
+    }
+}
